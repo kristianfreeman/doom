3ed1266eb4e1936da0a255da988e215d88770522
Kristian Freeman <kristian@kristianfreeman.com>
2020-05-03 11:32:24 -0500

    Initial commit

---
+++ config.el
@@ -0,0 +1,15 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+; Configured defaults from doom
+
+(setq user-full-name "Kristian Freeman"
+      user-mail-address "kristian@bytesized.xyz")
+(setq doom-font (font-spec :family "Dank Mono" :size 18))
+(setq doom-theme 'doom-one)
+(setq org-directory "~/Dropbox/org/")
+(setq display-line-numbers-type t)
+
+; Custom
+
+;; Set fullscreen on startup
+(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
